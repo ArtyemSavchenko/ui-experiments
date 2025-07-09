8fd91e4b06e9939b2c31749dfed8b9d27f69b1e5
Artyem Savchenko <a.savchenko89@yandex.ru>
2025-07-09 12:32:43 +0300

    feature: Изменил формирование классов модулей в зависимости от режима прод-дев

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,18 @@ import { optimizeSvg } from './vite-plugins/optimizeSvg';
 const ICONS_PATH = './src/shared/assets/monochrome-svg-icons';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr(), tsconfigPaths(), optimizeSvg(ICONS_PATH)],
+export default defineConfig(({ mode }) => {
+  const isProd = mode === 'production';
+
+  return {
+    plugins: [react(), svgr(), tsconfigPaths(), optimizeSvg(ICONS_PATH)],
+
+    css: {
+      modules: {
+        generateScopedName: isProd
+          ? '[hash:base64:8]'
+          : '[local]_[hash:base64:8]',
+      },
+    },
+  };
 });
